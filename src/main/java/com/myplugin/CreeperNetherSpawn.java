fe87940fdb3825dc3b1b6cf3136bb3bb43c76d73
Hung Nguyen <101444336+hungnguyennaz@users.noreply.github.com>
2024-06-29 02:44:40 +0700

    calculation should be async....right?

--- src/main/java/com/myplugin/CreeperNetherSpawn.java
+++ src/main/java/com/myplugin/CreeperNetherSpawn.java
@@ -9,6 +9,7 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitScheduler;
 
 import java.util.Random;
 
@@ -41,19 +42,25 @@ public class CreeperNetherSpawn extends JavaPlugin {
     }
 
     public void startCreeperSpawnTask() {
-        new BukkitRunnable() {
+        BukkitScheduler scheduler = Bukkit.getScheduler();
+        scheduler.runTaskTimerAsynchronously(this, new Runnable() {
             @Override
             public void run() {
                 for (Player player : Bukkit.getOnlinePlayers()) {
                     if (player.getWorld().getEnvironment() == World.Environment.NETHER) {
                         Location loc = getRandomLocationNear(player.getLocation());
-                        if (loc.getBlock().getType() == Material.AIR) {
-                            player.getWorld().spawnEntity(loc, EntityType.CREEPER);
-                        }
+                        scheduler.runTask(CreeperNetherSpawn.this, new Runnable() {
+                            @Override
+                            public void run() {
+                                if (loc.getBlock().getType() == Material.AIR) {
+                                    player.getWorld().spawnEntity(loc, EntityType.CREEPER);
+                                }
+                            }
+                        });
                     }
                 }
             }
-        }.runTaskTimer(this, 0L, 600L);
+        }, 0L, 600L);
     }
 
     public Location getRandomLocationNear(Location loc) {
